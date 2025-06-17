f53abdfe0d83e16bcc01ad2afac87483260de4d5
Sara França <sara.assessoria2015@gmail.com>
2025-06-16 23:06:58 -0300

    integração da aplicação no banco de dados

--- src/main/java/br/com/sfranca/screenmatch/model/Serie.java
+++ src/main/java/br/com/sfranca/screenmatch/model/Serie.java
@@ -1,17 +1,34 @@
 package br.com.sfranca.screenmatch.model;
 
-//import br.com.sfranca.screenmatch.service.ConsultaChatGPT;
 import br.com.sfranca.screenmatch.service.AwsTradutor;
-//import br.com.sfranca.screenmatch.service.ConsultaGemini;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
 
 import java.util.Optional;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
